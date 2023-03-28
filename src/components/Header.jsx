d56a30b769d543bf44cf36d17fcf3097c39d372f
CamiloIzquierdo <camiloi.99@hotmail.com>
2023-03-28 16:58:24 -0300

    fix mobile 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,40 +6,42 @@ function Header() {
     const [open, setOpen] = useState(false);
     return (
         <nav className="p-3 border-gray-200 rounded bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-            <div className="container flex items-center justify-center mx-auto flex-wrap md:flex-nowrap md:justify-between">
-                <Link to="/" className="flex items-center">
-                    <img
-                        src={Logo}
-                        className="h-6 mr-3 sm:h-10"
-                        alt="Flowbite Logo"
-                    />
-                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                        AppGod
-                    </span>
-                </Link>
-                <button
-                    data-collapse-toggle="navbar-solid-bg"
-                    type="button"
-                    className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                    aria-controls="navbar-solid-bg"
-                    aria-expanded="true"
-                    onClick={() => setOpen(!open)}
-                >
-                    <span className="sr-only">Open main menu</span>
-                    <svg
-                        className="w-6 h-6"
-                        aria-hidden="true"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
+            <div className="container items-center justify-center mx-auto flex-wrap md:flex-nowrap md:justify-between contents md:flex">
+                <div className="flex items-center justify-center">
+                    <Link to="/" className="flex items-center">
+                        <img
+                            src={Logo}
+                            className="h-6 mr-3 sm:h-10"
+                            alt="Flowbite Logo"
+                        />
+                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                            AppGod
+                        </span>
+                    </Link>
+                    <button
+                        data-collapse-toggle="navbar-solid-bg"
+                        type="button"
+                        className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        aria-controls="navbar-solid-bg"
+                        aria-expanded="true"
+                        onClick={() => setOpen(!open)}
                     >
-                        <path
-                            fillRule="evenodd"
-                            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                            clipRule="evenodd"
-                        ></path>
-                    </svg>
-                </button>
+                        <span className="sr-only">Open main menu</span>
+                        <svg
+                            className="w-6 h-6"
+                            aria-hidden="true"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                                clipRule="evenodd"
+                            ></path>
+                        </svg>
+                    </button>
+                </div>
                 <div
                     className={`md:block md:w-fit" id="navbar-solid-bg ${
                         open ? "" : "hidden"
